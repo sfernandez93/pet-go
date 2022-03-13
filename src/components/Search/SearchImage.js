e661557c7796c103552c257b62835b9a353b4c05
sfernandez93 <sabela.fernandez93@gmail.com>
2022-03-13 11:59:06 +0100

    feat: add style css buttons image

--- src/components/Search/SearchImage.js
+++ src/components/Search/SearchImage.js
@@ -1,8 +1,8 @@
 import { SearchContext } from "../../context/SearchContext";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import SearchButton from "./SearchButton"
-import { FaHeart, FaSadTear, FaSistrix, FaPlus } from "react-icons/fa";
+import SearchButton from "./SearchButton";
+import { FaHeart, FaSadTear, FaSistrix, FaPlus, FaUndo, FaGrinHearts } from "react-icons/fa";
 
 const SearchImage = () => {
   const { indexImages, petsAllData } = useContext(SearchContext);
@@ -11,26 +11,56 @@ const SearchImage = () => {
     petsAllData && petsAllData.length > 0 ? petsAllData[indexImages].uid : ""
   );
   return (
-      // <div className="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-      //   <img
-      //     src={
-      //       petsAllData && petsAllData.length > 0
-      //         ? petsAllData[indexImages].imagesUrl[
-      //             Object.keys(petsAllData[indexImages].imagesUrl)[0]
-      //           ]
-      //         : ""
-      //     }
-      //     // "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg"
-      //     alt="Collection of four insulated travel bottles on wooden shelf."
-      //     className="w-full h-full object-center object-cover"
-      //   ></img>
-      // </div>
-      <div
-      className="w-11/12 h-4/5 overflow-hidden relative justify-end  cursor-pointer capitalize flex flex-col rounded-md bg-red-500 z-50 object-cover text-white">
-      <img className="w-full h-full object-cover" src="https://t2.uc.ltmcdn.com/es/posts/8/3/6/como_saber_si_un_border_collie_es_puro_44638_600_square.jpg" alt="desktop" />
+    // <div className="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
+    //   <img
+    //     src={
+    //       petsAllData && petsAllData.length > 0
+    //         ? petsAllData[indexImages].imagesUrl[
+    //             Object.keys(petsAllData[indexImages].imagesUrl)[0]
+    //           ]
+    //         : ""
+    //     }
+    //     // "https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg"
+    //     alt="Collection of four insulated travel bottles on wooden shelf."
+    //     className="w-full h-full object-center object-cover"
+    //   ></img>
+    // </div>
+    <div className="w-11/12 h-4/5 overflow-hidden relative justify-end  cursor-pointer capitalize flex flex-col rounded-md bg-red-500 z-50 object-cover text-white">
+      <img
+        className="w-full h-full object-cover"
+        src="https://t2.uc.ltmcdn.com/es/posts/8/3/6/como_saber_si_un_border_collie_es_puro_44638_600_square.jpg"
+        alt="desktop"
+      />
       <div className="w-full bg-transparent-black absolute p-5 flex flex-col">
-        <span className= "text-3xl font-normal">Ron <span className="text-lg text-gray-400">3</span></span>
-        <span className="capitalize" >Madrid</span>
+        <span className= "text-3xl font-normal">Ron <span className="text-lg text-gray-400">Madrid</span></span>
+        {/* <span className="capitalize" >Madrid</span> */}
+        <div class="flex pt-15 items-center w-full justify-evenly">
+          <div
+            className="w-12 h-12 border-solid border-2 border-deepskyblue rounded-full flex items-center justify-center"
+            to={"/search"}
+          >
+            <div class="text-gray-300 hover:bg-gray-100 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              <FaUndo size={20} style={{ fill: "deepskyblue" }} />
+            </div>
+          </div>
+          <div
+            className="w-16 h-16 border-solid border-2 border-chartreuse rounded-full flex items-center justify-center"
+            to={"/upload"}
+          >
+            <div class="text-gray-300 hover:bg-gray-100 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              <FaSadTear size={30} style={{ fill: "chartreuse" }} />
+            </div>
+          </div>
+
+          <div
+            className="w-16 h-16 border-solid border-2 border-hotpink rounded-full flex items-center justify-center"
+            to={"/favorites"}
+          >
+            <div class="text-gray-300 hover:bg-gray-100 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              <FaGrinHearts size={30} style={{ fill: "hotpink" }} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,15 @@ module.exports = {
       colors: {
         "blue-violet": "#b885e4",
         "dark-violet": "#8d41c9",
-        "clear-blue": "#e0eefc",
+        "clear-blue": "#40cfe0",
         "clear-gray": "#e5e7eb",
         "marine-green": "#5a8fc46b",
-        "transparent-black": "#000000a3"
+        "transparent-black": "#000000a3",
+        "violet-pink": "#ea0bdb",
+        yellow: "#ffff00",
+        chartreuse: "#7fff00",
+        deepskyblue: "#00bfff",
+        hotpink: "#ff69b4",
       },
     },
   },
