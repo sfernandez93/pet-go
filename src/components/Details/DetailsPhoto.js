b9ca9b08197623044738a6d6681dda1c4e84ea14
sfernandez93 <sabela.fernandez93@gmail.com>
2022-03-14 18:28:09 +0100

    feat: hide slider details when only one photo

--- src/components/Details/DetailsPhoto.js
+++ src/components/Details/DetailsPhoto.js
@@ -3,7 +3,7 @@ import { DetailsContext } from "../../context/DetailsContext";
 import { useContext } from "react";
 
 const DetailsPhoto = ({ photo }) => {
-  const { incrementIndexImage, decrementIndexImage } =
+  const { incrementIndexImage, decrementIndexImage, detailPet } =
     useContext(DetailsContext);
 
   return (
@@ -11,7 +11,7 @@ const DetailsPhoto = ({ photo }) => {
       <img src={photo} className="rounded-t-lg object-center object-cover w-full h-72" />
       <button
         onClick={decrementIndexImage}
-        className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+        className={`${detailPet && Object.keys(detailPet.imagesUrl).length < 2 ? "hidden": ""} carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0`}
         type="button"
         data-bs-target="#carouselExampleCrossfade"
         data-bs-slide="prev"
@@ -20,7 +20,7 @@ const DetailsPhoto = ({ photo }) => {
       </button>
       <button
         onClick={incrementIndexImage}
-        className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+        className={`${detailPet && Object.keys(detailPet.imagesUrl).length < 2 ? "hidden": ""} carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0`}
         type="button"
         data-bs-target="#carouselExampleCrossfade"
         data-bs-slide="next"
