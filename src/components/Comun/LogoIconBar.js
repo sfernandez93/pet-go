6bbfd44ac58ce5b93a3732e308cacd2e256ebb7e
sfernandez93 <sabela.fernandez93@gmail.com>
2022-03-14 14:41:34 +0100

    feat: add NavLink to LogoIconBar

--- src/components/Comun/LogoIconBar.js
+++ src/components/Comun/LogoIconBar.js
@@ -1,12 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 const LogoIconBar = () => {
   return (
-    <div className="w-full flex justify-center items-center fixed top-0 gradient-background h-16">
+    <NavLink to="/search" className="w-full flex justify-center items-center fixed top-0 gradient-background h-16">
       <img
         className="h-3/4"
         src={require("../../images/output-logo-remove-bar-3.png")}
         alt="logo"
       ></img>
-    </div>
+    </NavLink>
   );
 };
 export default LogoIconBar;
