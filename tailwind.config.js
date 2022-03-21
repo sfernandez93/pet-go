05b9d849b47646f0013362803c750f86ee3fbc67
sfernandez93 <sabela.fernandez93@gmail.com>
2022-03-21 18:40:49 +0100

    fix: delete colors not used

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "blue-violet": "#b885e4",
-        "dark-violet": "#8d41c9",
         clearblue: "#c0d9ef",
-        "clear-gray": "#e5e7eb",
         "marine-green": "#5a8fc46b",
-        "transparent-black": "#000000a3",
-        "violet-pink": "#ea0bdb",
-        yellow: "#ffff00",
-        chartreuse: "#7fff00",
-        deepskyblue: "#00bfff",
-        hotpink: "#ff69b4",
-        "clear-ghostwhite": "#dbdbef",
-        "submit-green": "#66c4b0",
-        pastelblue:"#bbbbea",
-        transparentbg:"#fffffff2",
-        transparentgray:"#80808066",
-        "primary":"#16a48c87",
-        "lightseagreen":"#139d8d70"
-        // ""6ae0b2
+        transparentbg: "#fffffff2",
+        primary: "#16a48c87",
+        lightseagreen: "#139d8d70"
       },
     },
   },
